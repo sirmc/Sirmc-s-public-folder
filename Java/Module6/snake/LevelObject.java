f92742a6e45177a31a902e6bbb61f8117881e566
Sebastian Österlund <sirmcx@gmail.com>
2012-12-08 14:46:17 +0100

    Added module 6 and 7
    Module 6 is an implementation of Snake.
    Module 7 is a program which finds the longest path in a maze.

---
+++ Java/Module6/snake/LevelObject.java
@@ -0,0 +1,15 @@
+package snake;
+
+public class LevelObject {
+	int x;
+	int y;
+	int type;
+	
+	LevelObject(int type, int x, int y) {
+		this.type = type;
+		this.x = x;
+		this.y = y;
+		
+	}
+
+}
---
+++ Java/Module7/LevelObject.java
@@ -0,0 +1,13 @@
+public class LevelObject {
+	int x;
+	int y;
+	int type;
+	
+	
+	LevelObject(int type, int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.type= type;
+	}
+
+}
